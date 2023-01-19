bbaa9f0791ec5c261ae913676b95cd359b328771
ret7020 <ret7020@gmail.com>
2023-01-19 22:58:52 +0300

    Base server

--- SyncServer/server.cpp
+++ SyncServer/server.cpp
@@ -1,10 +1,27 @@
 #include "crow.h"
 
+bool check_file(std::string storage, std::string file_name)
+{
+    
+}
+
 int main()
 {
+    // Temp conf
+    std::string SERVER_STORAGE = "./Storage/";
+    
     crow::SimpleApp app;
     CROW_ROUTE(app, "/")([](){
-        return "Hello world";
+        return "Test";
+    });
+    CROW_ROUTE(app, "/get_file/<string>")([](std::string file_name){
+        std::cout << file_name << "\n";
+        return "File cont here";
+    });
+
+    CROW_ROUTE(app, "/sync/<string>")([](std::string file_name){
+        std::cout << file_name << "\n";
+        return "Uploading";
     });
     app.port(18080).multithreaded().run();
 }
